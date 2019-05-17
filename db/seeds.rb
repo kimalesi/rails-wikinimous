7afe97b5dca0210fbd60244c9881ab4f977ea011
Kim Alesi <kim.alesi@gmail.com>
2019-05-17 17:07:37 +0200

    rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+puts 'Creating 10 fake hipster paragraphs...'
+10.times do
+  article = Article.new(
+    title: Faker::Hipster.word,
+    content: Faker::Hipster.paragraphs
+  )
+  article.save!
+end
+puts 'Finished!'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
